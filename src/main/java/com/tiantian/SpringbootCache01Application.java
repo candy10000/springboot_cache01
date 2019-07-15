2e0add5296ccfe606903313ae2cdd6d03f53be40
“candy10000” <812294502@qq.com>
2019-07-15 15:36:48 +0800

    idea使用Git来版本控制

---
+++ src/main/java/com/tiantian/SpringbootCache01Application.java
@@ -0,0 +1,13 @@
+package com.tiantian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCache01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCache01Application.class, args);
+    }
+
+}
